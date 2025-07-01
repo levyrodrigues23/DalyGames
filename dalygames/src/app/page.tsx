818a916a55588f05af56b6886f0a1b1f9ee356d3
José Levy Rodrigues da Silva <joselevy@alu.ufc.br>
2025-06-30 22:58:55 -0300

    add new pages, configurations and the firts responsives

---
+++ dalygames/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center"> 
+    <h1>
+    teste
+    </h1>      
+    </div>
+  );
+}
