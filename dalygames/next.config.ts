fd6ab61d1b2c24405a12c75e77e1584ef8208ad8
José Levy Rodrigues da Silva <joselevy@alu.ufc.br>
2025-07-01 23:35:15 -0300

    feat: add fetch to api and design

--- dalygames/next.config.ts
+++ dalygames/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol: "https",
+        hostname: "sujeitoprogramador.com",
+      } //tive que criar como metodo de segurança, pois o Next.js não aceita imagens de outros sites por padrão
+
+    ]
+  }
 };
 
 export default nextConfig;
--- dalygames/src/app/page.tsx
+++ dalygames/src/app/page.tsx
@@ -1,11 +1,41 @@
+import { Container } from "@/components/container";
+import { GameProps } from "@/utils/types/game";
+import Link from "next/link";
 import Image from "next/image";
 
-export default function Home() {
+async function getDalyGame(){
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=game_day`)
+    return res.json();
+
+
+  } catch (error) {
+    throw new Error("Error fetching Daly Game");
+  }
+}
+
+export default async function Home() {
+const dalyGame: GameProps = await getDalyGame();
+
+
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center"> 
-    <h1>
-    teste
-    </h1>      
+    <div className="w-full"> 
+    <Container>
+      <h1 className="text-center font-bold text-xl mt-8 mb-5">Separamos um jogo exclusivo para voce!!!</h1>
+    <Link href={`/game/${dalyGame.id}`} className="flex flex-col items-center">
+      <section className="w-full bg-black rounded-lg ">
+        <Image src={dalyGame.image_url} 
+        alt={dalyGame.title} 
+        priority={true} 
+        quality={100} 
+        fill={true}
+        />
+      </section>
+    </Link>
+    </Container>
+
     </div>
   );
 }
